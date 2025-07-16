15a6a98c116da3da0eb4e3d447a57931244843f6
Arthur Calixto <88586876+arthur-calixto@users.noreply.github.com>
2025-07-15 21:34:35 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -298,7 +298,7 @@ Nome e assinatura do solicitante`;
 
                 // Despedida
                 doc.text('Atenciosamente,', margemEsquerda, posicaoY);
-                posicaoY += 50;
+                posicaoY += 15;
 
                 // Linha de assinatura
                 doc.line(margemEsquerda, posicaoY, margemEsquerda + 100, posicaoY);
